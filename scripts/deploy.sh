586dcd69c88a660a1bb8dce17ce43cc8b545f954
José Rato <rato.josepedro4@gmail.com>
2022-04-19 22:14:24 +0100

    adds grafana deployment to gcp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,8 @@ kubectl delete deployment trips-deployment
 kubectl delete deployment mongo-trips-deployment
 kubectl delete deployment keys-deployment
 kubectl delete deployment mongo-keys-deployment
+kubectl delete deployment prometheus-deployment
+kubectl delete deployment grafana-deployment
 } &> /dev/null
 
 # Start ingress controller (had to be done first because it is the one that takes the most time)
@@ -20,6 +22,10 @@ kubectl apply -f config/trips.yaml
 kubectl apply -f config/mongo-keys.yaml
 kubectl apply -f config/keys-configmap.yaml
 kubectl apply -f config/keys.yaml
+
+kubectl apply -f config/prometheus-cm.yaml
+kubectl apply -f config/prometheus.yaml
+kubectl apply -f config/grafana.yaml
 
 kubectl create secret tls ingress-secret --cert ingress/ingress.crt --key ingress/ingress.key
 
@@ -36,3 +42,6 @@ NGINX_INGRESS_IP=$(kubectl get service ingress-nginx-controller -n ingress-nginx
 export NGINX_INGRESS_IP
 
 echo External IP - "${NGINX_INGRESS_IP}"
+
+GRAFANA_IP=$(kubectl get service grafana-service -ojson | jq -r '.status.loadBalancer.ingress[].ip')
+echo Grafana IP - "${GRAFANA_IP}"

e69295cb3b23c55fa5dd8d6223787c867923dd66
José Rato <rato.josepedro4@gmail.com>
2022-04-20 11:01:30 +0100

    adds prometheus metrics to trips service

--- trip/swagger_server/__main__.py
+++ trip/swagger_server/__main__.py
@@ -12,6 +12,8 @@ from swagger_server import encoder
 from swagger_server.controllers.grpc_controller import TripsServicer
 from swagger_server.proto import trips_pb2_grpc
 
+from prometheus_flask_exporter import ConnexionPrometheusMetrics
+
 interceptors = [ExceptionToStatusInterceptor()]
 grpc_server = grpc.server(futures.ThreadPoolExecutor(max_workers=10), interceptors=interceptors)
 
@@ -20,6 +22,9 @@ def start_flask():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'NYC Trips'}, pythonic_params=True)
+    metrics = ConnexionPrometheusMetrics.for_app_factory()
+    metrics.info('app_info', 'Trips Service', version='1.0')
+    metrics.init_app(app)
     app.run(port=8080)
 
 

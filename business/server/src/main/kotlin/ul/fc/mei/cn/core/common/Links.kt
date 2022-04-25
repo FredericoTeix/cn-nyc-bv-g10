f5ed3d5170290f564e8a17a130cb1281779d335f
José Rato <rato.josepedro4@gmail.com>
2022-04-25 22:49:10 +0100

    fixes invalid endpoint paths

--- business/server/src/main/kotlin/ul/fc/mei/cn/core/common/Links.kt
+++ business/server/src/main/kotlin/ul/fc/mei/cn/core/common/Links.kt
@@ -2,6 +2,6 @@ package ul.fc.mei.cn.core.common
 
 object Links {
     const val BusinessById = "/business/{bid}"
-    const val Businesses = "/business/"
-    const val LoadBusinesses = "admin/business/load"
+    const val Businesses = "/business"
+    const val LoadBusinesses = "/admin/business/load"
 }

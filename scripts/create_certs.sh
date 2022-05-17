7e9759a5d0832b96310fd9c8b5041a0f789417d8
Daniel Dias <daniel.jose.dias@outlook.com>
2022-05-17 10:57:50 +0100

    changes script to create various certificates for all microservices in order to use TLS

--- scripts/create_certs.sh
+++ scripts/create_certs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-openssl req -nodes -newkey rsa:4096 -keyout ingress/ingress.key -out ingress/ingress.csr -subj "/CN=ingress"
-openssl x509 -req -days 365 -in ingress/ingress.csr -signkey ingress/ingress.key -out ingress/ingress.crt
-
+myArray=("ingress" "business" "keys" "trip" "value")
+for str in ${myArray[@]}; do
+    echo "Generating certificate for $str microservice"
+    openssl req -nodes -newkey rsa:4096 -keyout $str/$str.key -out $str/$str.csr -subj "/CN=$str" 
+    openssl x509 -req -days 365 -in $str/$str.csr -signkey $str/$str.key -out $str/$str.crt 
+    echo $'\n'
+done

75fd0712b304760ea5cbea041ade4b9406ef8585
Daniel Dias <daniel.jose.dias@outlook.com>
2022-05-24 22:12:03 +0100

    adds CI functionality to business with the required files

--- business/scripts/mongo-init.js
+++ business/scripts/mongo-init.js
@@ -1,21 +1,26 @@
+keysDb = db.getSiblingDB('business')
+testDb = db.getSiblingDB('business_test')
+
 let colName = "business"
-db = new Mongo().getDB(colName);
-db.createCollection(colName, {capped: false});
+let dbs = [keysDb, testDb]
 
-let id = db[colName].insert(
-    {
-        "name": "Shopping",
-        "city": "New York",
-        "address": "Some Address nº 12",
-        "location": {
-            "type": "Point",
-            "coordinates": [70.12314, 40.123123]
+dbs.forEach(db => {
+    db.createCollection(colName, {capped: false});
+    db[colName].insert(
+        {
+            "name": "Shopping",
+            "city": "New York",
+            "address": "Some Address nº 12",
+            "location": {
+                "type": "Point",
+                "coordinates": [70.12314, 40.123123]
+            }
         }
-    }
-);
-
-db[colName].createIndex({location: "2dsphere"})
-
-db[colName].find().forEach( place =>
-    db.businesses.deleteOne(place)
-)
+    );
+    
+    db[colName].createIndex({location: "2dsphere"})
+    
+    db[colName].find().forEach( place =>
+        db.businesses.deleteOne(place)
+    )
+})

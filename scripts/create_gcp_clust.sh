2f9b63274141907e5acd9a3b4d43b08987d8138f
José Rato <rato.josepedro4@gmail.com>
2022-06-01 00:02:28 +0100

    increases k8 cluster max-nodes and max-cpu
    decreases k8 trips deployment replicas

--- scripts/create_gcp_clust.sh
+++ scripts/create_gcp_clust.sh
@@ -25,13 +25,13 @@ gcloud container clusters create ${CLUSTER_NAME} --zone ${REGION} --no-enable-ba
   --cluster-version "1.21.11-gke.1100" --release-channel "stable" --machine-type "e2-standard-2" \
   --image-type "COS_CONTAINERD" --disk-type "pd-standard" --disk-size "30" --metadata disable-legacy-endpoints=true \
   --scopes=storage-ro,logging-write,monitoring-write,service-control,service-management,trace \
-  --max-pods-per-node "110" --num-nodes "1" --enable-autoscaling --min-nodes "0" --max-nodes "3" --logging=SYSTEM,WORKLOAD --monitoring=SYSTEM --enable-ip-alias \
+  --max-pods-per-node "110" --num-nodes "1" --enable-autoscaling --min-nodes "0" --max-nodes "4" --logging=SYSTEM,WORKLOAD --monitoring=SYSTEM --enable-ip-alias \
   --network "projects/${PROJECT}/global/networks/default" \
   --subnetwork "projects/${PROJECT}/regions/europe-west1/subnetworks/default" --no-enable-intra-node-visibility \
   --default-max-pods-per-node "110" \
   --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver \
   --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0 --enable-shielded-nodes \
-  --node-locations ${REGION} --enable-autoprovisioning --max-cpu 6 --max-memory 24
+  --node-locations ${REGION} --enable-autoprovisioning --max-cpu 8 --max-memory 24
 gcloud container clusters get-credentials ${CLUSTER_NAME} --zone ${REGION}
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user="${ACCOUNT}"
 

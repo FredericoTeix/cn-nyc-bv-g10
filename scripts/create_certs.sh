1a08bad8aeb832fa431f9a260cac49d0339d5aed
Rodrigo Alexandre <rdrslv00@gmail.com>
2022-06-01 06:57:23 -0700

    create certs no prompt

--- scripts/create_certs.sh
+++ scripts/create_certs.sh
@@ -10,8 +10,8 @@ done
 
 for str in ${myArray[@]}; do
     echo "Generating certificate for $str microservice"
-    openssl req -nodes -newkey rsa:4096 -keyout $str/$str.key -out $str/$str.csr -subj "/CN=$str" 
+    openssl req -nodes -newkey rsa:4096 -keyout $str/$str.key -out $str/$str.csr -subj "/CN=$str"
     openssl x509 -req -days 365 -in $str/$str.csr -signkey $str/$str.key -out $str/$str.crt
-    openssl pkcs12 -export -out $str/$str.p12 -inkey $str/$str.key -in $str/$str.crt -name $str
+    openssl pkcs12 -export -out $str/$str.p12 -inkey $str/$str.key -in $str/$str.crt -name $str -passout pass:
     echo $'\n'
 done

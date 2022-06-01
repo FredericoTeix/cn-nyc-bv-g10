0aa3f55daf0bb2f04a263b8277d5a5ea79ccf613
Rodrigo Alexandre <rdrslv00@gmail.com>
2022-06-01 13:08:08 -0700

    Modified deploy for cost optimization

--- scripts/create_gcp_clust.sh
+++ scripts/create_gcp_clust.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REGION="europe-west1-b"
+REGION="europe-west6-a"
 CLUSTER_NAME="cntrips-cluster-2"
 
 # Set project id if not specified yet
@@ -22,16 +22,16 @@ fi
 
 gcloud services enable cloudbuild.googleapis.com container.googleapis.com
 gcloud container clusters create ${CLUSTER_NAME} --zone ${REGION} --no-enable-basic-auth --preemptible \
-  --cluster-version "1.21.11-gke.1100" --release-channel "stable" --machine-type "e2-standard-2" \
+  --cluster-version "1.21.11-gke.1100" --release-channel "stable" --machine-type "n2-standard-2" \
   --image-type "COS_CONTAINERD" --disk-type "pd-standard" --disk-size "30" --metadata disable-legacy-endpoints=true \
   --scopes=storage-ro,logging-write,monitoring-write,service-control,service-management,trace \
-  --max-pods-per-node "110" --num-nodes "1" --enable-autoscaling --min-nodes "0" --max-nodes "4" --logging=SYSTEM,WORKLOAD --monitoring=SYSTEM --enable-ip-alias \
+  --max-pods-per-node "110" --num-nodes "2" --enable-autoscaling --min-nodes "0" --max-nodes "6" --logging=SYSTEM,WORKLOAD --monitoring=SYSTEM --enable-ip-alias \
   --network "projects/${PROJECT}/global/networks/default" \
-  --subnetwork "projects/${PROJECT}/regions/europe-west1/subnetworks/default" --no-enable-intra-node-visibility \
+  --subnetwork "projects/${PROJECT}/regions/europe-west6/subnetworks/default" --no-enable-intra-node-visibility \
   --default-max-pods-per-node "110" \
   --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver \
   --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0 --enable-shielded-nodes \
-  --node-locations ${REGION} --enable-autoprovisioning --max-cpu 8 --max-memory 32
+  --node-locations ${REGION} --enable-autoprovisioning --max-cpu 12 --max-memory 48
 gcloud container clusters get-credentials ${CLUSTER_NAME} --zone ${REGION}
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user="${ACCOUNT}"
 

da8c182ecaddb307881abe203da0a12ca0eebf25
José Rato <rato.josepedro4@gmail.com>
2022-06-08 00:12:46 +0100

    updates delivery action to build images in parallel

--- scripts/create_certs.sh
+++ scripts/create_certs.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-myArray=("ingress" "business" "keys" "trip" "value")
-for str in ${myArray[@]}; do
-    rm $str/$str.key
-    rm $str/$str.csr
-    rm $str/$str.crt
-    rm $str/$str.p12
+default_services=("ingress" "business" "keys" "trip" "value")
+services=("${@:-${default_services[@]}}")
+
+for svc in ${services[@]}; do
+    rm $svc/$svc.key
+    rm $svc/$svc.csr
+    rm $svc/$svc.crt
+    rm $svc/$svc.p12
 done 
 
-for str in ${myArray[@]}; do
-    echo "Generating certificate for $str microservice"
-    openssl req -nodes -newkey rsa:4096 -keyout $str/$str.key -out $str/$str.csr -subj "/CN=$str"
-    openssl x509 -req -days 365 -in $str/$str.csr -signkey $str/$str.key -out $str/$str.crt
-    openssl pkcs12 -export -out $str/$str.p12 -inkey $str/$str.key -in $str/$str.crt -name $str -passout pass:
+for svc in ${services[@]}; do
+    echo "Generating certificate for $svc microservice"
+    openssl req -nodes -newkey rsa:4096 -keyout $svc/$svc.key -out $svc/$svc.csr -subj "/CN=$svc"
+    openssl x509 -req -days 365 -in $svc/$svc.csr -signkey $svc/$svc.key -out $svc/$svc.crt
+    openssl pkcs12 -export -out $svc/$svc.p12 -inkey $svc/$svc.key -in $svc/$svc.crt -name $svc -passout pass:
     echo $'\n'
 done
